d5b320b43dbada3cc829b21ebf16a685832f1bd6
Luis Enrique Betancourt <lbetancourt894@gmail.com>
2024-11-23 17:43:33 -0600

    Menu de Inicio, funcionando

--- app/src/main/java/com/example/iacccess/Login.java
+++ app/src/main/java/com/example/iacccess/Login.java
@@ -2,13 +2,25 @@ package com.example.iacccess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
-
+    Button btnInicioSesion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnInicioSesion = findViewById(R.id.btnIniciarSesion);
+    }
+
+    public void abrirMenuPrincipal(View v){
+        Intent intent = new Intent(this, MenuPrincipal.class);
+        startActivity(intent);
+        finish();
     }
 }
