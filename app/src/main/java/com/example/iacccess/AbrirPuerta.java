a1ab3ed1817c49e5aaaeca37d93e3a6349043163
Velasco Campos Cristian Daniel <21151014@aguascalientes.tecnm.mx>
2024-12-03 23:26:18 -0600

    correcciones de diseño

--- app/src/main/java/com/example/iacccess/AbrirPuerta.java
+++ app/src/main/java/com/example/iacccess/AbrirPuerta.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Spinner;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,5 +62,6 @@ public class AbrirPuerta extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_abrir_puerta, container, false);
+
     }
 }
